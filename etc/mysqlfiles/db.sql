b33b1699c89efe8643c04dd8dbc125805e47f434
Mushu <dragon.mushu@hotmail.com>
2020-01-31 17:15:15 +0100

    docker-compose, composer to api and deploy container

---
+++ etc/mysqlfiles/db.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS api_example;
+CREATE DATABASE IF NOT EXISTS api_example CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- CREATE USER 'api_user'@'localhost' identified by 'api_password';
+-- GRANT ALL on api_example.* to 'api_user'@'localhost';
+USE api_example;
+
+DROP TABLE IF EXISTS person;
+CREATE TABLE IF NOT EXISTS person (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    firstparent_id INT DEFAULT NULL,
+    secondparent_id INT DEFAULT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (firstparent_id)
+        REFERENCES person(id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (secondparent_id)
+        REFERENCES person(id)
+        ON DELETE SET NULL
+) ENGINE=INNODB;
+
+INSERT INTO person
+    (id, firstname, lastname, firstparent_id, secondparent_id)
+VALUES
+    (1, 'Krasimir', 'Hristozov', null, null),
+    (2, 'Maria', 'Hristozova', null, null),
+    (3, 'Masha', 'Hristozova', 1, 2),
+    (4, 'Jane', 'Smith', null, null),
+    (5, 'John', 'Smith', null, null),
+    (6, 'Richard', 'Smith', 4, 5),
+    (7, 'Donna', 'Smith', 4, 5),
+    (8, 'Josh', 'Harrelson', null, null),
+    (9, 'Anna', 'Harrelson', 7, 8);
